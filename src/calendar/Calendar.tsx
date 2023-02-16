076c68c2d82834363ff95d6013dc4c69054fa786
Steven Vergenz <1882376+stevenvergenz@users.noreply.github.com>
2023-02-15 22:17:26 -0800

    Add correct dating

--- src/calendar/Calendar.tsx
+++ src/calendar/Calendar.tsx
@@ -1,16 +1,51 @@
-import { Week } from './Week';
+import { DateTime } from 'luxon';
+
+import Day from './Day';
 import './Calendar.css';
 
-export default function Calendar() {
-	const weeks = [0,1,2,3,4].map(i => <Week index={i} />);
+export default function Calendar()
+{
 	return (
 		<table className="calendar">
 			<thead>
-				<Week header={true} index={-1} />
+				<tr className="week">
+					<th>Sunday</th>
+					<th>Monday</th>
+					<th>Tuesday</th>
+					<th>Wednesday</th>
+					<th>Thursday</th>
+					<th>Friday</th>
+					<th>Saturday</th>
+				</tr>
 			</thead>
 			<tbody>
-				{weeks}
+				{generateDayGrid()}
 			</tbody>
 		</table>
 	);
+}
+
+function generateDayGrid(): JSX.Element[]
+{
+	const today = DateTime.now();
+	const firstDay = DateTime.fromObject({ year: today.year, month: today.month, day: 1});
+
+	const weeks: JSX.Element[] = [];
+	for (let i = 0; i < 5; i++)
+	{
+		const days: JSX.Element[] = [];
+		for (let j = 0; j < 7; j++)
+		{
+			const gridIndex = 7 * i + j;
+			const date = firstDay.plus({ days: gridIndex - (firstDay.weekday % 7) })
+			days.push(<Day gridIndex={7 * i + j} date={date} isOverflow={date.month != today.month} />);
+		}
+
+		weeks.push(
+			<tr className="week">
+				{days}
+			</tr>);
+	}
+
+	return weeks;
 }
