c133dc4718d391b315385747716436bb7d42fb8a
Steven Vergenz <1882376+stevenvergenz@users.noreply.github.com>
2023-02-15 22:17:30 -0800

    Implement weather forecast

--- src/weather/Api.ts
+++ src/weather/Api.ts
@@ -1,3 +1,5 @@
+import { DateTime } from 'luxon';
+
 type ApiResponse<T> =
 {
 	data: T | undefined;
@@ -5,21 +7,50 @@ type ApiResponse<T> =
 
 export type WeatherReading =
 {
-	dt: number;
-	main: { temp: number; };
-	weather: { icon: string; }[];
+	/** Timestamp of the reading */
+	dt: DateTime;
+	/** The temperature in F */
+	main: { temp?: number; temp_min?: number, temp_max?: number };
+	/** The general weather status */
+	weather: { icon: string; iconNum: number; };
+	/** Percent chance of precipitation */
 	pop?: number;
-	sys?: { pod: string; };
 };
 
 export async function getCurrentWeather(lat: string, long: string): Promise<WeatherReading | undefined>
 {
 	const res = await fetch(`/api/weather/current?lat=${lat}&long=${long}`);
 	if (res.ok) {
-		const resData = await res.json() as ApiResponse<WeatherReading>;
-		return resData.data;
+		const resData = await res.json();
+		if (resData.data) {
+			resData.data.dt = DateTime.fromSeconds(resData.data.dt);
+			resData.data.weather = resData.data.weather[0];
+			resData.data.weather.iconNum = parseInt(resData.data.weather.icon.slice(0,2));
+		}
+		return resData.data as WeatherReading | undefined;
 	}
 	else {
 		throw new Error(`Failure fetching current weather: ${res.statusText}`);
+	}
+}
+
+export async function getForecastWeather(lat: string, long: string): Promise<WeatherReading[] | undefined>
+{
+	const res = await fetch(`/api/weather/forecast?lat=${lat}&long=${long}`);
+	if (res.ok) {
+		const resData = await res.json();
+		if (resData.data)
+		{
+			for (const reading of resData.data)
+			{
+				reading.dt = DateTime.fromSeconds(reading.dt);
+				reading.weather = reading.weather[0];
+				reading.weather.iconNum = parseInt(reading.weather.icon.slice(0,2), 10);
+			}
+		}
+		return resData.data as WeatherReading[] | undefined;
+	}
+	else {
+		throw new Error(`Failure fetching forecast weather: ${res.statusText}`);
 	}
 }
--- src/weather/index.tsx
+++ src/weather/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { DateTime } from 'luxon';
+import { DateTime, Duration } from 'luxon';
 
 import WeatherTile from './WeatherTile';
-import { getCurrentWeather, WeatherReading } from './Api';
+import { getCurrentWeather, getForecastWeather, WeatherReading } from './Api';
 import useAutoRefreshingState from '../utils/useAutoRefreshingState';
 
 import './Weather.css';
@@ -15,45 +15,67 @@ type WeatherParams = {
 
 export default function Weather(params: WeatherParams): JSX.Element
 {
-	const [curWeather, setCurWeather] = useAutoRefreshingState<WeatherReading | undefined>(
+	const [curWeather] = useAutoRefreshingState<WeatherReading | undefined>(
 		undefined,
-		() => getCurrentWeather(params.lat, params.long).then(r => { console.log(r); return r; }),
+		() => getCurrentWeather(params.lat, params.long),
 		[params.lat, params.long],
 		1000 * 60 * 60
 	);
 
-	const fakeData: WeatherReading = {
-		dt: DateTime.now().toUnixInteger(),
-		main: { temp: 50 },
-		weather: [{ icon: "50d" }]
-	};
+	const [forecast] = useAutoRefreshingState<WeatherReading[] | undefined>(
+		[],
+		() => getForecastWeather(params.lat, params.long),
+		[params.lat, params.long],
+		1000 * 60 * 60 * 8
+	);
 
 	return (
 		<div className="weather">
 			<WeatherTile big={true} data={curWeather} />
 			<div className="weather-rows">
 				<div className="weather-row">
-					<WeatherTile data={fakeData} />
-					<WeatherTile data={fakeData} />
-					<WeatherTile data={fakeData} />
-					<WeatherTile data={fakeData} />
+					<WeatherTile data={forecast?.[0]} />
+					<WeatherTile data={forecast?.[1]} />
+					<WeatherTile data={forecast?.[2]} />
 				</div>
 				<div className="weather-row">
-					<WeatherTile data={fakeData} />
-					<WeatherTile data={fakeData} />
-					<WeatherTile data={fakeData} />
-					<WeatherTile data={fakeData} />
+					<WeatherTile data={findExtendedForecast(forecast, 1)} />
+					<WeatherTile data={findExtendedForecast(forecast, 2)} />
+					<WeatherTile data={findExtendedForecast(forecast, 3)} />
 				</div>
 			</div>
-			{/*<div className="weather-row">
-				<WeatherTile name="9 AM" forecast="H: 67" icon="" />
-				<WeatherTile name="10 AM" forecast="H: 67" icon="" />
-				<WeatherTile name="11 AM" forecast="H: 67" icon="" />
-				<WeatherTile name="12 PM" forecast="H: 67" icon="" />
-				<WeatherTile name="1 PM" forecast="H: 67" icon="" />
-				<WeatherTile name="2 PM" forecast="H: 67" icon="" />
-				<WeatherTile name="3 PM" forecast="H: 67" icon="" />
-			</div>*/}
 		</div>
 	);
+}
+
+function findExtendedForecast(readings: WeatherReading[] | undefined, daysOut: number): WeatherReading | undefined
+{
+	if (!readings) return undefined;
+
+	let targetDay = DateTime.now().plus({ days: daysOut });
+
+	const aggReading: WeatherReading = {
+		dt: targetDay,
+		main: { temp_min: undefined, temp_max: undefined },
+		weather: { icon: "00d", iconNum: 0 },
+		pop: 0
+	};
+
+	for (let i = 0; i < readings.length; i++)
+	{
+		if (readings[i].dt.hasSame(targetDay, 'day'))
+		{
+			aggReading.main.temp_min
+				= Math.min(aggReading.main.temp_min ?? Infinity, readings[i].main.temp ?? 0);
+			aggReading.main.temp_max
+				= Math.max(aggReading.main.temp_max ?? -Infinity, readings[i].main.temp ?? 0);
+			if (readings[i].weather.iconNum > aggReading.weather.iconNum) {
+				aggReading.weather.icon = readings[i].weather.icon;
+				aggReading.weather.iconNum = readings[i].weather.iconNum;
+			}
+			aggReading.pop = Math.max(aggReading.pop ?? 0, readings[i].pop ?? 0);
+		}
+	}
+
+	return aggReading;
 }
