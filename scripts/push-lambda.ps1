9754d8793c4e1519d1c7781a93aed9b27d4f44aa
Steven Vergenz <1882376+stevenvergenz@users.noreply.github.com>
2023-02-15 22:17:31 -0800

    Finally got time zones figured out

--- scripts/push-lambda.ps1
+++ scripts/push-lambda.ps1
@@ -1,7 +1,7 @@
-& "$PSScriptRoot/../node_modules/.bin/tsc.ps1" --project "$PSScriptRoot/../tsconfig.lambda.json"
 Push-Location "$PSScriptRoot/../notion-recurring-lambda"
-Remove-Item -Recurse node_modules -ErrorAction Ignore
+Remove-Item -Recurse -ErrorAction Ignore node_modules, index.js
 & npm install --production
+& "$PSScriptRoot/../node_modules/.bin/tsc.ps1" --project "$PSScriptRoot/../tsconfig.lambda.json"
 Compress-Archive -Force -Path index.js, node_modules -DestinationPath notion-recurring-lambda.zip
 & aws lambda update-function-code --function-name NotionRecurringEvents --zip-file "fileb://notion-recurring-lambda.zip"
 Pop-Location
