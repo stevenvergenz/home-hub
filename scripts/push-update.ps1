89e396cb07d7055be9016849ad6429154ee3c632
Steven Vergenz <1882376+stevenvergenz@users.noreply.github.com>
2023-02-15 22:17:27 -0800

    Deploy automation

---
+++ scripts/push-update.ps1
@@ -0,0 +1,5 @@
+rm -recurse build/*, serverBuild/*
+npm run build
+scp -r package.json package-lock.json scripts/ build/ serverBuild/ steven@192.168.1.25:/home/steven/home-hub
+ssh steven@192.168.1.25 'chmod +x /home/steven/home-hub/scripts/process-update.sh'
+ssh steven@192.168.1.25 '/home/steven/home-hub/scripts/process-update.sh'
