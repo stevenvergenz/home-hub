37f4a5e6d45ae698bd98ba1249ec868cd5b922ca
Steven Vergenz <1882376+stevenvergenz@users.noreply.github.com>
2023-06-04 22:02:18 -0700

    Fix IP

--- scripts/push-update.sh
+++ scripts/push-update.sh
@@ -1,5 +1,5 @@
 #!/bin/env sh
 rm -r build/* serverBuild/*
 npm run build
-scp -r config.json package.json package-lock.json scripts/ build/ serverBuild/ steven@192.168.1.151:/home/steven/home-hub
-ssh steven@192.168.1.151 'chmod +x /home/steven/home-hub/scripts/*.sh && /home/steven/home-hub/scripts/process-update.sh'
+scp -r config.json package.json package-lock.json scripts/ build/ serverBuild/ steven@192.168.1.25:/home/steven/home-hub
+ssh steven@192.168.1.25 'chmod +x /home/steven/home-hub/scripts/*.sh && /home/steven/home-hub/scripts/process-update.sh'
